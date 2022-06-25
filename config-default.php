42549df50368202d1ebcd7ceaa417d5d3d4df0f3
Rick Clark <quixrick@gmail.com>
2022-06-25 10:34:37 -0600

    Add default config file

---
+++ config-default.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+$settings = array();
+
+
+
+
+
+
+// [db] Database Connection Parameters
+$settings['db']['db_host'] = 'localhost';
+$settings['db']['db_name'] = 'testdb';
+$settings['db']['db_user'] = 'xxxxxxxx';
+$settings['db']['db_pass'] = 'xxxxxxxxxxxx';
+$settings['db']['db_port'] = 27017;
